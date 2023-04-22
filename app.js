c65b1575836452b49acb59e2d9d3c1a44ac12c2d
cuong <front.cuong@gmail.com>
2023-04-22 19:54:06 +0700

    full

--- app.js
+++ app.js
@@ -51,33 +51,9 @@ app.use("/api", orderRoutes);
 app.get((req, res) => {
     res.status(404).send('Sorry, resource not found');
 });
-
-
-// app.use(async (req, res, next) => {
-//     const allProducts = [
-//         ...productsData.womensProducts.map(p => ({ ...p, productType: 2 })),
-//         ...productsData.mensProducts.map(p => ({ ...p, productType: 1 })),
-//         ...productsData.topsProducts.map(p => ({ ...p, productType: 3 })),
-//         ...productsData.bottomsProducts.map(p => ({ ...p, productType: 4 })),
-//         ...productsData.accessoriesProducts.map(p => ({ ...p, productType: 5 })),
-//         ...productsData.outerweasProducts.map(p => ({ ...p, productType: 6 }))
-//     ];
-//     try {
-//         await Promise.all(allProducts.map(product =>
-//             AllProducts.create({
-//                 title: product.title,
-//                 images: product.images,
-//                 price: product.price,
-//                 productType: product.productType,
-//             })
-//         ));
-//         console.log('All products created successfully1');
-//         next();
-//     } catch (err) {
-//         console.error('Error creating products:', err);
-//         next(err);
-//     }
-// });
+app.get('/', (req, res) => {
+    res.send('Hello from Vercel!')
+})
 
 sequelize
     // .sync({ force: true })
